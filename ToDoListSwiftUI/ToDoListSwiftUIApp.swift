b0f9364aae9bece093b12cfe86625dba928543df
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-11-04 21:13:07 +0300

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Олег Федоров on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
